1d6ad7f6d7a1002ebe856c32b94a4299fd974011
tanliangbang <648103576@qq.com>
2017-09-20 17:33:23 +0800

    '2017-9-20'

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,21 +1,52 @@
 <template>
   <div class="hello">
+
+    <mt-navbar v-model="selected">
+      <mt-tab-item id="1">前端资源</mt-tab-item>
+      <mt-tab-item id="2">后端资源</mt-tab-item>
+    </mt-navbar>
+
+    <!-- tab-container -->
+    <mt-tab-container v-model="selected">
+      <mt-tab-container-item id="1">
+        <mt-cell v-for="n in 10" :title="'content ' + n" />
+      </mt-tab-container-item>
+      <mt-tab-container-item id="2">
+        <mt-cell v-for="n in 4" :title="'content ' + n" />
+      </mt-tab-container-item>
+      <mt-tab-container-item id="3">
+        <mt-cell v-for="n in 6" :title="'content ' + n" />
+      </mt-tab-container-item>
+    </mt-tab-container>
+
+
+
+
+
     <mt-tabbar v-model="selected">
       <mt-tab-item id="tab1">
-
-        tab1
+        <img slot="icon" src="../assets/logo.png">
+        学习资源
       </mt-tab-item>
       <mt-tab-item id="tab2">
-        tab2
+        <img slot="icon" src="../assets/logo.png">
+        IT资讯
       </mt-tab-item>
       <mt-tab-item id="tab3">
-        tab3
+        <img slot="icon" src="../assets/logo.png">
+        交流社区
       </mt-tab-item>
       <mt-tab-item id="tab4">
-        tab4
+        <img slot="icon" src="../assets/logo.png">
+        个人中心
       </mt-tab-item>
     </mt-tabbar>
+
+
   </div>
+
+
+
 </template>
 
 <script>
@@ -26,6 +57,7 @@
   name: 'hello',
   data () {
     return {
+      selected: '1',
       msg: 'Welcome to Your Vue.js App'
     }
   },
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Mint from 'mint-ui';
-Vue.use(Mint);
+import MintUI from 'mint-ui';
+import 'mint-ui/lib/style.css';
+
+Vue.use(MintUI);
 
 
 Vue.config.productionTip = false
